5f274ee0778bafc237d7098c9b80ba37c0bc174c
N1097098 <eshan.shukla2022@my.ntu.ac.uk>
2022-12-19 11:31:27 +0000

    Tree view added onto screen1 controller

--- app/src/main/java/org/openjfx/app/Screen1Controller.java
+++ app/src/main/java/org/openjfx/app/Screen1Controller.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.util.ResourceBundle;
+import java.util.Vector;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
@@ -21,6 +22,8 @@ import javafx.stage.Stage;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 /**
  * FXML Controller class
  *
@@ -29,38 +32,28 @@ import javafx.scene.control.TextArea;
 
 
 public class Screen1Controller {
-
     @FXML
-    private MenuItem closeFile;
-
-    @FXML
-    private ListView<String> myListView;
-
-    @FXML
-    private MenuItem shareFile;
-
-    @FXML
-    private MenuItem saveFile;
-
+    private TreeView<String> myTreeView;
     @FXML
     private MenuItem myprofile;
-
     @FXML
     private MenuItem logOut;
-
     @FXML
-    private MenuItem addFile;
-
+    private MenuItem upload;
     @FXML
     private MenuItem createFile;
-    
+    @FXML
+    private MenuItem createDirectory;
+    @FXML
+    private MenuItem save;
+    @FXML
+    private MenuItem share;
+    @FXML
+    private MenuItem close;
     @FXML
     private TextArea txtArea;
-    
-    String[] files = {};
-    
+    String[] fileName = {};
     private static String username;
-    
     private static Stage stageOfTextArea;
    
     public void initialize() {
@@ -70,18 +63,52 @@ public class Screen1Controller {
        String path = dirPath + this.username;
        try{
            File dir = new File(path);
-           this.files = dir.list();
-           for(String s : files){
-               System.out.println(s);
+           this.fileName = dir.list();
+           TreeItem<String> root = new TreeItem<>("Your Folders");
+           Vector<TreeItem<String>> branches = createView(fileName, path + "/");
+           int len = branches.size();
+           for(int i = 0; i < len; ++i){
+               root.getChildren().add(branches.get(i));
            }
+         myTreeView.setRoot(root);
        } catch(SecurityException ex){
            System.out.println("error");
        } catch(NullPointerException ex){
            System.out.println("which error");
        }
-       myListView.getItems().addAll(files);
        
         
+    }
+    
+    private Vector<TreeItem<String>> createView(String[] fileName, String rootPath){
+        Vector<TreeItem<String>> branches = new Vector<TreeItem<String>>();
+        String tempPath = rootPath;
+        for(String s : fileName){
+            tempPath = rootPath + s;
+            try{
+                File f = new File(tempPath);
+                if(f.isDirectory()){
+                    TreeItem<String> root = new TreeItem<String>(s);
+                    fileName = f.list();
+                    Vector<TreeItem<String>> tempBranch = createView(fileName, tempPath + "/");
+                    int len = tempBranch.size();
+                    for(int i = 0; i < len; ++i){
+                        root.getChildren().add(tempBranch.get(i));
+                    }
+                    branches.add(root);
+                }else{
+                    TreeItem<String> branch = new TreeItem<String>(s);
+                    branches.add(branch);
+                }
+                
+            }catch(SecurityException ex){
+                System.out.println("error");
+            } catch(NullPointerException ex){
+                System.out.println("which error");
+            }
+            
+        }
+        return branches;
     }
 
     public void setUsername(String user) {
@@ -89,12 +116,12 @@ public class Screen1Controller {
     }
     
     @FXML
-    private void addfile(ActionEvent event) {
+    private void onClickUpload(ActionEvent event) {
 
     }
 
     @FXML
-    private void createfile(ActionEvent event) throws IOException {
+    private void onClickCreateFile(ActionEvent event) throws IOException {
         Stage stage = new Stage();
         FXMLLoader fxml = new FXMLLoader(getClass().getResource("filename.fxml"));
         Parent root = fxml.load();
@@ -104,16 +131,26 @@ public class Screen1Controller {
         stage.show();
         stageOfTextArea = (Stage)txtArea.getScene().getWindow();
     }
+    
+    @FXML
+    private void onClickCreateDirectory(ActionEvent event){
+        
+    }
 
     @FXML
-    private void savefile(ActionEvent event) {
+    private void onClickSave(ActionEvent event) {
 
     }
 
 
     @FXML
-    private void closefile(ActionEvent event) {
+    private void onClickShare(ActionEvent event) {
 
+    }
+    
+    @FXML
+    private void onClickClose(ActionEvent event){
+        
     }
 
     @FXML
